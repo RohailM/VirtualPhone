df249c131b11e64c39ee84fe8bc1f800e9a75741
rohai <rohai@10.0.0.250>
2021-12-09 19:57:37 -0500

    Commit 2 Changelog:
    - added icon functionality
    - attempted to make Frame class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		/*Frame screen = new Frame(true, false, 450, 720, "Virtual Phone");
+		screen.set_icon_of_frame(screen, "src/images/phone_icon.png")
+		*/
+		
 		// Creating a basic GUI window, to which components can be added onto
 		JFrame screen = new JFrame();
 		
@@ -10,17 +16,22 @@ public class Main {
 		screen.setVisible(true);
 		
 		// adding a title to the application that displays at the top of the window
-		screen.setTitle("Interactive User Interface");
+		screen.setTitle("Virtual Phone");
+
+		// using javax.swing.ImageIcon and creating an ImageIcon
+		ImageIcon phoneIcon = new ImageIcon("src/images/phone_icon.png");
+		
+		// change icon of frame to phoneIcon
+		screen.setIconImage(phoneIcon.getImage());
 		
 		// setting the size of the frame to 720x720, first parameter is x-dimension and second parameter is y-dimension
-		screen.setSize(720,720);
+		screen.setSize(450,720);
 		
 		// making it so when the x icon, which is located at top right of frame, is clicked, the application is exited.
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// prevents users from resizing the frame
-		screen.setResizable(false);
-
+		screen.setResizable(false);	
 	}
 
 }
