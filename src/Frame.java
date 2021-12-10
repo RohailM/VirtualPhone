5bdb9702990ceb00c93bd71a46a7d696aef3df29
FurqaanK <95870515+FurqaanK@users.noreply.github.com>
2021-12-10 11:02:03 -0500

    Commit Changelog 4:
    - Added alignCenter method
    - Added setIconOfFrame method
    - Painted the initial rounded rectangle of the phone.

--- src/Frame.java
+++ src/Frame.java
@@ -1,20 +1,33 @@
+import java.awt.*;
 import javax.swing.*;
+import java.awt.Color;
 
 public class Frame extends JFrame {
 	
 	JPanel panel;
 	JLabel label;
 
-	Frame (String title, int x, int y) {
+	Frame (String title, int x, int y, boolean resize, boolean visible) {
 		super(title);
 		setSize(x, y);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(resize);
+		setVisible(visible);
+		getContentPane().setBackground(new Color(21,63,130));
 	}
-	/*
-	public void set_icon_of_frame(JFrame window, String path) {
-		this.icon_of_frame = path; 
+	
+	public void alignCenter() {
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+	}
+	
+	public void setIconOfFrame(String path) {
 		ImageIcon icon = new ImageIcon(path);
-		window.setIconImage(icon.getImage());
+		this.setIconImage(icon.getImage());
 	}
-	*/
+	
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawRoundRect(30, 50, 390, 650, 50, 50);
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Frame window = new Frame("Virtual Phone", 450, 720);
-		window.setVisible(true);
-		window.setResizable(false);	
+		Frame window = new Frame("Virtual Phone", 450, 720, false, true);
+		window.alignCenter();
+		window.setIconOfFrame("src/images/phone_icon.png");
+		
 	}
 
 }
