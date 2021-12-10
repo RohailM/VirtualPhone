3845e9751b31ec6e26314db0707161b64650b46d
FurqaanK <95870515+FurqaanK@users.noreply.github.com>
2021-12-10 09:48:58 -0500

    Commit 3 Changelog:
    - Made a Frame class

--- src/Frame.java
+++ src/Frame.java
@@ -1,29 +1,20 @@
-/*import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class Frame {
-	protected boolean is_visible;
-	protected boolean is_resizable;
-	protected int x_dimension;
-	protected int y_dimension;
-	protected String title_of_frame;
-	protected String icon_of_frame;
-
-	Frame (boolean vis, boolean resize, int x, int y, String title) {
-		this.is_visible = vis;
-		this.is_resizable = resize;
-		this.x_dimension = x;
-		this.y_dimension = y;
-		this.title_of_frame = title;
-	}
+public class Frame extends JFrame {
 	
+	JPanel panel;
+	JLabel label;
+
+	Frame (String title, int x, int y) {
+		super(title);
+		setSize(x, y);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	/*
 	public void set_icon_of_frame(JFrame window, String path) {
 		this.icon_of_frame = path; 
 		ImageIcon icon = new ImageIcon(path);
 		window.setIconImage(icon.getImage());
 	}
-	
-	public static void main (String[] args) {
-		JFrame screen = new JFrame();
-	}
-}*/
+	*/
+}
--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,9 @@ import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
-		/*Frame screen = new Frame(true, false, 450, 720, "Virtual Phone");
-		screen.set_icon_of_frame(screen, "src/images/phone_icon.png")
-		*/
-		
-		// Creating a basic GUI window, to which components can be added onto
-		JFrame screen = new JFrame();
-		
-		// makes it so frame is visible and can be seen
-		screen.setVisible(true);
-		
-		// adding a title to the application that displays at the top of the window
-		screen.setTitle("Virtual Phone");
-
-		// using javax.swing.ImageIcon and creating an ImageIcon
-		ImageIcon phoneIcon = new ImageIcon("src/images/phone_icon.png");
-		
-		// change icon of frame to phoneIcon
-		screen.setIconImage(phoneIcon.getImage());
-		
-		// setting the size of the frame to 720x720, first parameter is x-dimension and second parameter is y-dimension
-		screen.setSize(450,720);
-		
-		// making it so when the x icon, which is located at top right of frame, is clicked, the application is exited.
-		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// prevents users from resizing the frame
-		screen.setResizable(false);	
+		Frame window = new Frame("Virtual Phone", 450, 720);
+		window.setVisible(true);
+		window.setResizable(false);	
 	}
 
 }
